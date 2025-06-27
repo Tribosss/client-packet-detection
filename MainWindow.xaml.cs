4dfa26e00019d7509b5ca347d8d82e8b4ee18765
PKyes44 <rugate23@gmail.com>
2025-06-27 20:15:22 +0900

    feat: detect keyword in packet

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,14 +2,7 @@
 using SharpPcap.LibPcap;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using PacketDotNet;
 
 namespace client_packet_detection
 {
@@ -18,6 +11,13 @@ namespace client_packet_detection
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private string[] keywords = [
+            "HelloWorld",
+            "Hello",
+            "YES",
+        ];
+
         public MainWindow()
         {
             InitializeComponent();
@@ -31,7 +31,30 @@ namespace client_packet_detection
 
         void Device_OnPacketArrival(object s, PacketCapture e)
         {
-            Console.WriteLine(e.GetPacket());
+            byte[] rawBytes = e.GetPacket().Data;
+            Packet packet = Packet.ParsePacket(e.GetPacket().LinkLayerType, rawBytes);
+
+            EthernetPacket? ether = packet.Extract<EthernetPacket>();
+            if (ether == null) return;
+            IPPacket? ip = packet.Extract<IPPacket>();
+            if (ip == null) return;
+            TcpPacket? tcp = packet.Extract<TcpPacket>();
+            if (tcp == null) return;
+
+            byte[] payload = tcp.PayloadData;
+            if (payload == null || payload.Length <= 0) return;
+
+            string text = Encoding.ASCII.GetString(rawBytes);
+
+            foreach (string keyword in keywords)
+            {
+                if (text.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) < 0) continue;
+
+                Console.WriteLine($"Detected Keyword: {keyword}");
+                Console.WriteLine(text);
+                Console.WriteLine("======================");
+                break;
+            }
         }
     }
 }
